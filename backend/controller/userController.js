8adca63a2dd287267132ba94276dc3340438a4be
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-23 22:22:01 +0530

    create new user and store in db

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,17 @@
-const createUser = (req, res) => {
-  res.json({ user: req.body });
+// User model
+const User = require("../models/userModel");
+
+const createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //   to create new Record inside db
+  const newUser = new User({ name, email, password });
+  //_id is created by mongoose
+
+  //   to save in db
+  await newUser.save();
+
+  res.json({ user: newUser });
 };
 
 module.exports = { createUser };
