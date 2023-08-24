521c3d47ae9c3cbae7071d7bd97326490a313ec1
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-24 18:19:55 +0530

    hash password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt");
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -16,6 +18,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+//this code is a middleware function that triggers before
+//saving a user document. It checks if the password field
+//has been modified, and if so, it hashes the new or updated
+//password using the bcrypt library before storing it in the document.
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
