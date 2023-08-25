910a49d8fd3860b15cc4966c7c05c082557aefef
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-25 16:20:19 +0530

    create email verification token schema

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -1,9 +1,9 @@
 const { check, validationResult } = require("express-validator");
 
 exports.userValidator = [
+  //only when the condition fails message will be shown
   check("name").trim().not().isEmpty().withMessage("Name is missing"),
-  check("email"),
-  normalizeEmail().isEmail().withMessage("Email is invalid"),
+  check("email").normalizeEmail().isEmail().withMessage("Email is invalid"),
   check("password")
     .trim()
     .not()
@@ -13,7 +13,10 @@ exports.userValidator = [
     .withMessage("Password must be 8 to 20 characters long"),
 ];
 
+// function is used to gather the validation errors from the
+//  request after applying validation rules to the incoming data.
 exports.validate = (req, res, next) => {
+  // used to extract the validation errors as an array
   const result = validationResult(req).array();
   if (result.length) {
     return res.json({ error: result[0].msg });
