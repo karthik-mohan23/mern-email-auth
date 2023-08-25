331a8fff317315b428a288c6e6f794cbd6d1ba30
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-25 11:09:51 +0530

    validating users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 // controller
 const { createUser } = require("../controller/userController");
+// validator
+const { userValidator, validate } = require("../middlewares/validator");
 
 const router = express.Router();
 
-router.post("/sign-up", createUser);
+router.post("/sign-up", userValidator, validate, createUser);
 
 module.exports = router;
